29c0d7640d8b8289f454f270c915e8b5e9cf770f
ben.marengo <ben.marengo@just-eat.com>
2018-04-02 17:59:57 +0100

    overloaded constructor to create BaseGeoWKT objects from a wkt string

--- ocean_efficiency/legacy_model/GeoWKT.py
+++ ocean_efficiency/legacy_model/GeoWKT.py
@@ -1,10 +1,46 @@
+import regex
+
+multi_wkt_pattern = regex.compile('([A-Z]*\((?>[^\(\)]+|(?R))*\))')
+single_wkt_pattern = regex.compile('(\w*)\((.*)\)')
+lon_lat_pattern = regex.compile('(\d+\.?\d*) (\d+\.?\d*)')
 
 
 class BaseGeoWKT(object):
     wkt_tag = None
 
-    def __init__(self, obj):
+    def __init__(self, obj, wkt_tag=''):
         self.obj = obj
+        self.wkt_tag = wkt_tag
+
+    @classmethod
+    def from_wkt(cls, wkt_str):
+        wkt_match = single_wkt_pattern.findall(wkt_str.upper())
+        if wkt_match:
+            obj = cls._from_wkt(wkt_match[0][0].strip(), wkt_match[0][1])
+        else:
+            raise NotImplementedError()
+
+        return cls(obj)
+
+    @classmethod
+    def _from_wkt(cls, wkt_tag, wkt_str):
+        obj = []
+        wkt_elements = multi_wkt_pattern.findall(wkt_str)
+        if wkt_elements:
+            for wkt_element in wkt_elements:
+                wkt_element_match = single_wkt_pattern.findall(wkt_element)[0]
+                wkt_element_tag = wkt_element_match[0].strip()
+                wkt_element_data = wkt_element_match[1]
+                if wkt_element_tag in WKT_TAG_CLASS:
+                    obj.append(cls._from_wkt(wkt_element_tag, wkt_element_data))
+                elif wkt_element_tag == '' and wkt_tag == 'COMPOUNDCURVE':
+                    obj.append(cls._from_wkt('LINESTRING', wkt_element_data))
+                else:
+                    raise NotImplementedError()
+        else:
+            points = [Point(p) for p in lon_lat_pattern.findall(wkt_str)]
+            obj = WKT_TAG_CLASS[wkt_tag](points)
+        return obj
 
     @property
     def wkt(self):
@@ -21,12 +57,10 @@ class Point(BaseGeoWKT):
     p = Point([0, 0])
     p.wkt = (0 0)
     """
-    wkt_tag = ''
-
-    def __init__(self, obj):
+    def __init__(self, obj, wkt_tag=''):
         if type(obj) not in (list, tuple) and len(obj) != 2:
             raise ValueError('Only supply list or tuple')
-        super(Point, self).__init__(obj)
+        super(Point, self).__init__(obj, wkt_tag)
         self.x = obj[0]
         self.y = obj[1]
 
@@ -40,57 +74,67 @@ class Point(BaseGeoWKT):
 
 class LineString(BaseGeoWKT):
     """
-    ls = LineString([[0, 0], [1, 1], [1, 0]])
+    ls = LineString.from_lon_lat_list([[0, 0], [1, 1], [1, 0]])
     ls = LineString([Point([0, 0]), Point([1, 1]), Point([1, 0])])
     ls.wkt = (0 0, 1 1, 1 0)
     """
-    wkt_tag = ''
 
-    def __init__(self, obj):
-        if isinstance(obj[0], list):
-            obj = [Point(v) for v in obj]
+    def __init__(self, obj, wkt_tag=''):
         super(LineString, self).__init__(obj)
+        self.wkt_tag = wkt_tag
+
+    @classmethod
+    def from_lon_lat_list(cls, lon_lat_list, wkt_tag=''):
+        data = [Point(l) for l in lon_lat_list]
+        return cls(data, wkt_tag)
 
 
 class CircularString(BaseGeoWKT):
     """
-    cs = CircularString([[0, 0], [1, 1], [1, 0]])
+    cs = CircularString.from_lon_lat_list([[0, 0], [1, 1], [1, 0]])
     cs = CircularString([Point([0, 0]), Point([1, 1]), Point([1, 0])])
     cs.wkt = CIRCULARSTRING(0 0, 1 1, 1 0)
     """
-    wkt_tag = 'CircularString'
+    def __init__(self, obj, wkt_tag='CircularString'):
+        super(CircularString, self).__init__(obj, wkt_tag)
 
-    def __init__(self, obj):
-        if isinstance(obj[0], list):
-            obj = [Point(v) for v in obj]
-        super(CircularString, self).__init__(obj)
+    @classmethod
+    def from_lon_lat_list(cls, lon_lat_list, wkt_tag='CircularString'):
+        data = [Point(l) for l in lon_lat_list]
+        return cls(data, wkt_tag)
 
 
 class CompoundCurve(BaseGeoWKT):
     """
-    cc = CompoundCurve( cs, ls )
+    cc = CompoundCurve( [cs, ls] )
     cc.wkt = COMPOUNDCURVE( CIRCULARSTRING(0 0, 1 1, 1 0), (1 0, 0 1) )
     """
-    wkt_tag = 'CompoundCurve'
-
-    def __init__(self, obj):
-        if type(obj) not in (list, tuple) and len(obj) != 2:
+    def __init__(self, obj, wkt_tag='CompoundCurve'):
+        if type(obj) not in (list, tuple):
             raise ValueError('Only supply list or tuple')
 
         for o in obj:
             if type(o) not in (CircularString, LineString):
                 raise ValueError('Only supply CircularString or LineString')
 
-        super(CompoundCurve, self).__init__(obj)
+        super(CompoundCurve, self).__init__(obj, wkt_tag)
 
+
+WKT_TAG_CLASS = {
+    'POINT': Point,
+    'LINESTRING': LineString,
+    'CIRCULARSTRING': CircularString,
+    'COMPOUNDCURVE': CompoundCurve,
+}
 
 if __name__ == '__main__':
-    ls = LineString([[0, 0], [1, 1], [1, 0]])
-    cs = CircularString([[0, 0], [1, 1], [1, 0]])
+    ls = LineString.from_lon_lat_list([[0, 0], [1, 1], [1, 0]])
+    cs = CircularString.from_lon_lat_list([[0, 0], [1, 1], [1, 0]])
     cc = CompoundCurve([cs, ls])
     print(ls.wkt)
     print(cs.wkt)
     print(cc.wkt)
+    cc = CompoundCurve.from_wkt(cc.wkt)
 
 
 
